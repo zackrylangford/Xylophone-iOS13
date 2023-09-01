4745a2aeaf3c4a70f798e625d8d514181ea29829
Zackry Langford <zackrylangford@gmail.com>
2023-08-31 22:19:14 -0400

    completed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,53 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
 
-}
+    @IBAction func keyPressed(_ sender: UIButton) {
+        var soundName: String
+            switch sender.tag {
+            case 1:
+                soundName = "C"
+            case 2:
+                soundName = "D"
+            case 3:
+                soundName = "E"
+            case 4:
+                soundName = "F"
+            case 5:
+                soundName = "G"
+            case 6:
+                soundName = "A"
+            case 7:
+                soundName = "B"
+            default:
+                return
+            }
+            print("I got pressed. \(soundName) button")
+            playSound(fileName: soundName, fileExtension: "wav")
+        }
+        
+    }
+    
+    var audioPlayer: AVAudioPlayer?
+
+    func playSound(fileName: String, fileExtension: String) {
+        if let url = Bundle.main.url(forResource: fileName, withExtension: fileExtension) {
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: url)
+                audioPlayer?.play()
+            } catch {
+                print("Couldn't load the audio file.")
+            }
+        }
+    }
+
+
 
